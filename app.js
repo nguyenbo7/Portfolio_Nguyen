b1a46770d3b8196bd4920247b49a2eab7032584a
Nguyen Chau <nguyenchaubo7@gmail.com>
2022-06-25 22:31:03 -0700

    Task 3: Working on About section 	1. Add the basic structure of img swiper. 	2. Include title, tags img. Improve: add the hover effect on each img to show the text content.

---
+++ app.js
@@ -0,0 +1,12 @@
+/*==================== SWIPER DISCOVER ====================*/
+let swiper = new Swiper(".about__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
